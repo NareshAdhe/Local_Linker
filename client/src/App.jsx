6d1ce4dbf309b938e9f21b63589262229615b9a2
NareshAdhe <nareshadhe01@gmail.com>
2025-03-12 11:01:29 +0530

    changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full sm:max-w-7xl min-h-screen bg-gray-200 mx-auto">
+      App
+    </div>
+  );
 };
 
 export default App;
