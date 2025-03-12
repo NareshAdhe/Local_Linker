9a3265ef2d0bcc2325cdd6f2d859cac0331542d3
NareshAdhe <nareshadhe01@gmail.com>
2025-03-12 11:44:09 +0530

    changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="w-full sm:max-w-7xl min-h-screen bg-gray-200 mx-auto">
-      App
+    <div className="w-full sm:max-w-7xl min-h-screen bg-white mx-auto border-2 border-gray-200">
+      <Navbar />
+      <div>App</div>
     </div>
   );
 };
