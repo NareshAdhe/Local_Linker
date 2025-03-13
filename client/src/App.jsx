1d00d33610612d004f2712fc7bd62bde25bc7a9c
ANKANJAGTAP <157878894+ANKANJAGTAP@users.noreply.github.com>
2025-03-13 14:28:31 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import SecPage from "./pages/SecPage";
+import SecPage from "./pages/secpage";
 import Hero from "./components/Hero";
-
-import Hero from "./components/Hero";
+import Thirdpage from "./pages/thirdpage";
 const App = () => {
   return (
     <>
-      <div className="w-full sm:max-w-7xl min-h-screen bg-white mx-auto border-2 border-gray-200">
+      <div className="w-full sm:max-w-[80%] min-h-screen bg-white mx-auto">
         <Navbar />
         <Hero />
         <SecPage />
+        <Thirdpage />
       </div>
-      <div className="w-full sm:max-w-7xl min-h-screen bg-gray-200 mx-auto"></div>
     </>
   );
 };
 
 export default App;
-4;
