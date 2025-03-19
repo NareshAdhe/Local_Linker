3261599cd174d84876233b5769232e1778de75b1
NareshAdhe <nareshadhe01@gmail.com>
2025-03-19 16:35:54 +0530

    Completed

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,3 +1,7 @@
 import Redis from "ioredis";
-
-export default new Redis();
+export default process.env.NODE_ENV === "production"
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: "127.0.0.1",
+      port: 6379,
+    });
--- server/config/socket.js
+++ server/config/socket.js
@@ -8,7 +8,9 @@ const initSocket = (server) => {
     cors: {
       origin: process.env.FRONTEND_URL || "http://localhost:5173",
       credentials: true,
+      methods: ["GET", "POST"],
     },
+    transports: ["websocket", "polling"],
   });
 
   io.on("connection", (socket) => {
