174ea7b20150133db0cb716a8eac15f506813181
NareshAdhe <nareshadhe01@gmail.com>
2025-03-19 17:20:08 +0530

    Completed

--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -19,6 +19,22 @@ const Context = ({ children }) => {
   const [chatUsers, setChatUsers] = useState([]);
 
   useEffect(() => {
+    const newSocket = io(
+      backendURI || "http://localhost:4000",
+      {
+        withCredentials: true,
+        transports: ["websocket", "polling"],
+      }
+    );
+
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [backendURI]);
+
+  useEffect(() => {
     const fetchChatUsers = async () => {
       try {
         const { data } = await axios.get(
@@ -84,22 +100,6 @@ const Context = ({ children }) => {
       socket.off("updateChatUsers");
     };
   }, [socket, users]);
-
-  useEffect(() => {
-    const newSocket = io(
-      backendURI || "http://localhost:4000",
-      {
-        withCredentials: true,
-        transports: ["websocket", "polling"],
-      }
-    );
-
-    setSocket(newSocket);
-
-    return () => {
-      newSocket.disconnect();
-    };
-  }, []);
 
   useEffect(() => {
     if (localStorage.getItem("authToken")) {
