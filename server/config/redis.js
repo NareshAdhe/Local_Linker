6f0968198d465879c4d59a170c208085c3644d76
NareshAdhe <nareshadhe01@gmail.com>
2025-04-09 16:45:40 +0530

    completed docker setup

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,7 +1,8 @@
 import Redis from "ioredis";
-export default process.env.NODE_ENV === "production"
-  ? new Redis(process.env.REDIS_URL)
-  : new Redis({
-      host: "127.0.0.1",
-      port: 6379,
-    });
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "redis",
+  port: 6379,
+});
+
+export default redis;
